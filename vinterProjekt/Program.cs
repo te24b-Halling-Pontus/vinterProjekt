150d684fa487c0fce79db82b70077fccc691a298
te24b Halling Pontus <pontushalling08@gmail.com>
2025-12-08 11:06:21 +0100

    Jag har byt ut resurser mot pengar (mycket enklare)

--- vinterProjekt/Program.cs
+++ vinterProjekt/Program.cs
@@ -1,84 +1,81 @@
-﻿Dictionary<string, int> resources = new Dictionary<string, int>();
+﻿// Dictionary<string, int> resources = new Dictionary<string, int>();
 
-resources.Add("Money", 0);
-resources.Add("Stone", 0);
-resources.Add("Iron", 0);
-resources.Add("Coal", 0);
-resources.Add("Diamond", 0);
+// resources.Add("Money", 0);
+// resources.Add("Stone", 0);
+// resources.Add("Iron", 0);
+// resources.Add("Coal", 0);
+// resources.Add("Diamond", 0);
+// Hard to do, maybe later.
+int money = 0;
 
 Dictionary<string, (int Amount, int price, float OPS)> building = new Dictionary<string, (int, int, float)>(); //OPS = Ores per second of course. 
 
+building.Add("Mining drill lv1", (0, 10, 0.1f));
 
-static void HavestingResorces(Dictionary<string, int> resources)
+static void HavestingResorces(int money, Dictionary<string, (int Amount, int price, float OPS)> buldings)
 {
     ConsoleKey pressedKey = Console.ReadKey(true).Key;
     if (pressedKey == ConsoleKey.Spacebar)
     {
-        int i = Random.Shared.Next(101);
-        if (i >= 50)
-        {
-            resources["Stone"] += 1;
-        }
-        else if (i < 50 && i >= 20)
-        {
-            resources["Coal"] += 1;
-        }
-        else if (i < 20 && i > 1)
-        {
-            resources["Iron"] += 1;
-        }
-        else if (i == 1)
-        {
-            resources["Diamond"] += 1;
-        }
+        money++;
         Console.Clear();
         Console.WriteLine("Type [H] for hub");
-        Console.WriteLine("Stone\tCoal\tIron\tDiamond");
-        Console.WriteLine($"{resources["Stone"]}\t{resources["Coal"]}\t{resources["Iron"]}\t{resources["Diamond"]}\t");
-        HavestingResorces(resources);
+        HavestingResorces(money, buldings);
     }
     else if (pressedKey == ConsoleKey.H)
     {
-        Hub(resources);
+        Hub(money, buldings);
     }
     else
     {
-        HavestingResorces(resources);
+        HavestingResorces(money, buldings);
     }
 }
-static void Hub(Dictionary<string, int> resources)
+static void Hub(int money, Dictionary<string, (int Amount, int price, float OPS)> buldings)
 {
     ConsoleKey pressedKey = Console.ReadKey(true).Key;
     if (pressedKey == ConsoleKey.U)
     {
-        UpgradeShop(resources);
+        UpgradeShop(money);
     }
     else if (pressedKey == ConsoleKey.B)
     {
-        BuildingShop(resources);
+        BuildingShop(money, buldings);
     }
-    Hub(resources);
+    Hub(money, buldings);
 }
-static void UpgradeShop(Dictionary<string, int> resources)
+static void UpgradeShop(int money)
 {
-    Console.WriteLine($"${resources["Money"]}");
+    Console.WriteLine(money);
 
 }
 // ska nog sätta i hop dem om jag någonsin gör raylib
-static void BuildingShop(Dictionary<string, int> resources)
+static void BuildingShop(int money, Dictionary<string, (int Amount, int price, float OPS)> buldings)
 {
     ConsoleKey pressedKey = Console.ReadKey(true).Key;
-    Console.WriteLine("");
     int witchBuilding = 1;
     if (pressedKey == ConsoleKey.UpArrow)
     {
-        if (witchBuilding)
-        witchBuilding++;
+        if (witchBuilding >= 5)
+        {
+            Console.WriteLine("You are at the higest point alredy");
+        }
+        else
+        {
+            witchBuilding++;
+        }
     }
     else if (pressedKey == ConsoleKey.DownArrow)
     {
-        witchBuilding--;
+        if(witchBuilding >= 1)
+        {
+            Console.WriteLine("You are at the lowest point alredy");
+        }
+        else
+        {
+            witchBuilding--;
+        }
     }
 }
-HavestingResorces(resources);
+HavestingResorces(money, building);
 Console.ReadLine();
