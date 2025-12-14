cf12a0777402f311c8673039a9dda3c3edccfced
Pontus hallig <pontushalling08@gmail.com>
2025-12-14 16:12:20 +0100

    Jag har gjort lite mer på building shopen och byt med dictionaryn så att nycken är ett ID istället för namn och att man med ID kan få ut namnent och mycket merr med hjälp av tuples

--- vinterProjekt/Program.cs
+++ vinterProjekt/Program.cs
@@ -8,31 +8,35 @@
 // Hard to do, maybe later.
 int money = 0;
 
-Dictionary<string, (int Amount, int price, float OPS)> building = new Dictionary<string, (int, int, float)>(); //OPS = Ores per second of course. 
+Dictionary<int, (string Name, int Amount, int price, float OPS)> building = new Dictionary<int, (string, int, int, float)>(); //OPS = Ores per second of course. 
 
-building.Add("Mining drill lv1", (0, 10, 0.1f));
+building.Add(1, ("Employe", 0, 10, 0.1f));
+building.Add(2, ("Employew", 0, 10, 0.1f));
 
-static void HavestingResorces(int money, Dictionary<string, (int Amount, int price, float OPS)> buldings)
+static void HavestingResorces(int money, Dictionary<int, (string Name, int Amount, int price, float OPS)> building)
 {
     ConsoleKey pressedKey = Console.ReadKey(true).Key;
     if (pressedKey == ConsoleKey.Spacebar)
     {
         money++;
         Console.Clear();
-        Console.WriteLine("Type [H] for hub");
-        HavestingResorces(money, buldings);
+        Console.WriteLine(money);
+        Console.WriteLine("Press [H] for hub");
+        HavestingResorces(money, building);
     }
     else if (pressedKey == ConsoleKey.H)
     {
-        Hub(money, buldings);
+        Hub(money, building);
     }
     else
     {
-        HavestingResorces(money, buldings);
+        HavestingResorces(money, building);
     }
 }
-static void Hub(int money, Dictionary<string, (int Amount, int price, float OPS)> buldings)
+static void Hub(int money, Dictionary<int, (string Name, int Amount, int price, float OPS)> building)
 {
+    Console.Clear();
+    Console.WriteLine("Press [U] for upgrade, [B] for bildning shop, [I] for information, [Space] to go back to clicker");
     ConsoleKey pressedKey = Console.ReadKey(true).Key;
     if (pressedKey == ConsoleKey.U)
     {
@@ -40,9 +44,16 @@ static void Hub(int money, Dictionary<string, (int Amount, int price, float OPS)
     }
     else if (pressedKey == ConsoleKey.B)
     {
-        BuildingShop(money, buldings);
+        BuildingShop(money, building);
     }
-    Hub(money, buldings);
+    else if (pressedKey == ConsoleKey.Spacebar)
+    {
+        HavestingResorces(money, building);
+    }
+    else
+    {
+        Hub(money, building);
+    }
 }
 static void UpgradeShop(int money)
 {
@@ -50,30 +61,45 @@ static void UpgradeShop(int money)
 
 }
 // ska nog sätta i hop dem om jag någonsin gör raylib
-static void BuildingShop(int money, Dictionary<string, (int Amount, int price, float OPS)> buldings)
+static void BuildingShop(int money, Dictionary<int, (string Name, int Amount, int price, float OPS)> building)
 {
-    ConsoleKey pressedKey = Console.ReadKey(true).Key;
     int witchBuilding = 1;
-    if (pressedKey == ConsoleKey.UpArrow)
+    while (true)
     {
-        if (witchBuilding >= 5)
+        ConsoleKey pressedKey = Console.ReadKey(true).Key;
+        Console.Clear();
+        if (pressedKey == ConsoleKey.UpArrow)
         {
-            Console.WriteLine("You are at the higest point alredy");
+            if (witchBuilding == 1)
+            {
+                Console.WriteLine("You are at the higest point alredy");
+            }
+            else
+            {
+                witchBuilding--;
+            }
         }
-        else
+        else if (pressedKey == ConsoleKey.DownArrow)
         {
-            witchBuilding++;
+            if (witchBuilding == building.Count)
+            {
+                Console.WriteLine("You are at the lowest point alredy");
+            }
+            else
+            {
+                witchBuilding++;
+            }
         }
-    }
-    else if (pressedKey == ConsoleKey.DownArrow)
-    {
-        if(witchBuilding >= 1)
+        for (int i = 1; i < building.Count + 1; i++)
         {
-            Console.WriteLine("You are at the lowest point alredy");
-        }
-        else
-        {
-            witchBuilding--;
+            if (witchBuilding == i)
+            {
+                Console.WriteLine($">{building[i].Name}<");
+            }
+            else
+            {
+                Console.WriteLine(building[i].Name);
+            }
         }
     }
 }
